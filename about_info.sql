6785b35150c5d57901f6995146a4d3e2d78f09bd
rafidazahra <126293935+rafidazahramahiranih@users.noreply.github.com>
2024-05-25 10:34:18 +0700

    Database-phpmyadmin

---
+++ about_info.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 25, 2024 at 03:34 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portofolio_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about_info`
+--
+
+CREATE TABLE `about_info` (
+  `id` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `profile` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `address` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `about_info`
+--
+
+INSERT INTO `about_info` (`id`, `name`, `profile`, `email`, `address`) VALUES
+(1, 'Rafida Zahra MH', 'Graphic Designer', 'zrafida60@gmail.com', 'Sukabumi, Jawabarat');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about_info`
+--
+ALTER TABLE `about_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about_info`
+--
+ALTER TABLE `about_info`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
